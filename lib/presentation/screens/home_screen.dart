dd9f701984c2f8cd1c5976dc7eb62e59e7180c71
subrota <subrotadebnath1807@gmail.com>
2022-04-22 02:03:11 +0600

    Counter Bloc created and written and implemented

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_experiments/presentation/busness_logic/counter_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void dispose() {
+    counterBloc.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+        body: SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          StreamBuilder<int>(
+              initialData: 0,
+              stream: counterBloc.counterStream,
+              builder: (context, snapshot) {
+                return Text(snapshot.data.toString());
+              }),
+          ElevatedButton(
+            onPressed: () {
+              counterBloc.counterIncrement();
+            },
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.orange,
+              shape: const CircleBorder(),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
